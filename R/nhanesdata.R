8ec1f614bb05c7a790d8f4ee6378b9f79d8d5ce6
Dane Van Domelen <vandomed@gmail.com>
2019-02-09 13:34:32 -0500

    Added 1 file

--- R/nhanesdata.R
+++ R/nhanesdata.R
@@ -7,7 +7,7 @@
 #' Package: \tab nhanesdsata \cr
 #' Type: \tab Package \cr
 #' Version: \tab 1.1.1 \cr
-#' Date: \tab 2019-02-08 \cr
+#' Date: \tab 2019-02-09 \cr
 #' License: \tab GPL-3 \cr
 #' }
 #'

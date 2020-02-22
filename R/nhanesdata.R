03f65bb1d5dde209fa6c33d4e91905f68860d9e6
Dane Van Domelen <vandomed@gmail.com>
2020-02-22 16:47:25 -0500

    Trigger

--- R/nhanesdata.R
+++ R/nhanesdata.R
@@ -7,7 +7,7 @@
 #' Package: \tab nhanesdsata \cr
 #' Type: \tab Package \cr
 #' Version: \tab 1.1.1 \cr
-#' Date: \tab 2019-02-09 \cr
+#' Date: \tab 2020-02-22 \cr
 #' License: \tab GPL-3 \cr
 #' }
 #'
